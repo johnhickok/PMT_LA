db8781adca57a27aac5eb2218aa2969e2fef1cf4
John Hickok <jhickok2011@gmail.com>
2021-10-19 14:53:51 -0700

    Update add_pcls1.sql

--- CAMS Plus/add_pcls1.sql
+++ CAMS Plus/add_pcls1.sql
@@ -9,9 +9,6 @@ user_posttype,
 user_geo_city,
 user_situszip,
 user_situsfulladdress,
-ST_SetSRID(ST_MakePoint(
-	user_centoid_x::double precision, 
-	user_centoid_y::double precision
-    ),2229) as geom
+ST_GeometryFromText(WKT, 2229) as geom
 from public.parcel_geocode_results
 where score = 0;
